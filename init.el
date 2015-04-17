875b16d9902ae38b9d9cb15cd70ba835ac494a3b
Pratik Vyas <m@pd.io>
2015-04-17 18:52:15 +0530

    _

--- init.el
+++ init.el
@@ -1,6 +1,22 @@
 ;; This sets up the load path so that we can override it
-(package-initialize t)
+(package-initialize)
 (setq package-enable-at-startup nil)
 (require 'org)
 (require 'ob-tangle)
 (org-babel-load-file (expand-file-name "~/.emacs.d/pd.org"))
+;(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+;  '(smart-tab-completion-functions-alist
+;    (quote
+; 	((emacs-lisp-mode . lisp-complete-symbol)
+; 	 (text-mode . dabbrev-completion)
+; 	 (Python . company-complete-common-or-cycle)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
