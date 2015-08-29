112b5d2aa2fb2c043950a33a64d12ea82b4f9f19
Pratik Vyas <m@pd.io>
2015-08-29 12:26:46 -0700

    most probably moving to spacemacs

--- init.el
+++ init.el
@@ -20,3 +20,20 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+	("68cca1c0563918960cdd98f16164fb7c43ffc4c44ad6d97e10eda9cd684790bc" default)))
+ '(ledger-reports
+   (quote
+	(("mosaic" "ledger register Mosaic")
+	 ("balance" "ledger --depth 3")
+	 ("bal" "ledger -f %(ledger-file) bal")
+	 ("reg" "ledger -f %(ledger-file) reg")
+	 ("payee" "ledger -f %(ledger-file) reg @%(payee)")
+	 ("account" "ledger -f %(ledger-file) reg %(account)"))))
+ '(show-paren-mode t))
